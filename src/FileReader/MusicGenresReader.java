5658b91248c3407f70139960ddefc61ea8fce6a5
Maxime <vegxer@mail.ru>
2021-09-30 12:33:04 +0300

    "MusicGenresReader" class have been added for reading music genres from .json file

---
+++ src/FileReader/MusicGenresReader.java
@@ -0,0 +1,62 @@
+package FileReader;
+
+import Music.MusicBand;
+import Music.MusicGenre;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MusicGenresReader extends Reader {
+    public MusicGenresReader(String fileName) throws FileNotFoundException {
+        super.setFileName(fileName);
+    }
+
+    @Override
+    public Object readFile() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        JSONArray jsonGenres = (JSONArray)(((JSONObject)parser.parse(
+                new FileReader(super.fileName))).get("genres"));
+        ArrayList<MusicGenre> genres = new ArrayList<>();
+
+        for (Object obj : jsonGenres) {
+            JSONObject jsonGenre = (JSONObject)((JSONObject)obj).get("genre");
+            MusicGenre genre = new MusicGenre();
+            genre.setName((String)jsonGenre.get("name"));
+            genre.setMusicBands(getMusicBands(jsonGenre));
+
+            genres.add(genre);
+        }
+
+        if (genres.isEmpty())
+            throw new IllegalArgumentException("Genres are not found");
+
+        return genres;
+    }
+
+    private ArrayList<MusicBand> getMusicBands(JSONObject jsonGenre) {
+        JSONArray jsonBands = (JSONArray)jsonGenre.get("bands");
+        ArrayList<MusicBand> musicBands = new ArrayList<>();
+
+        for (Object band : jsonBands) {
+            JSONObject jsonBand = (JSONObject)band;
+
+            MusicBand musicBand = new MusicBand();
+            musicBand.setName((String)jsonBand.get("name"));
+            musicBand.setCountry((String)jsonBand.get("country"));
+            musicBand.setActivateYear(Integer.parseInt((String)jsonBand.get("year")));
+
+            musicBands.add(musicBand);
+        }
+
+        if (musicBands.isEmpty())
+            throw new IllegalArgumentException("Empty array of bands");
+
+        return musicBands;
+    }
+}
