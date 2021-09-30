f46294d385ff41212b4a80d6bc11d3b533dfd472
Maxime <vegxer@mail.ru>
2021-09-30 12:31:32 +0300

    "XmlToJsonFileConverter" class have been added, that converts music bands .xml file to music genres .json file

---
+++ src/FileConverter/XmlToJsonFileConverter.java
@@ -0,0 +1,37 @@
+package FileConverter;
+
+import FileReader.Reader;
+import FileWriter.Writer;
+import Music.MusicBand;
+import Music.MusicGenre;
+import org.json.simple.parser.ParseException;
+
+import javax.management.modelmbean.XMLParseException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.transform.TransformerException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class XmlToJsonFileConverter extends FileConverter {
+    public XmlToJsonFileConverter(String fileName) throws FileNotFoundException {
+        super.setFileName(fileName);
+    }
+
+    @Override
+    public void convert(String jsonFileName)
+            throws IOException, XMLStreamException, XMLParseException,
+            ParseException, ParserConfigurationException, TransformerException {
+        if (!Reader.getExtension(jsonFileName).equals("json"))
+            throw new IllegalArgumentException("Неверное расширение файла");
+
+        Reader xmlReader = Reader.create(super.fileName);
+
+        ArrayList<MusicBand> musicBands = (ArrayList<MusicBand>)xmlReader.readFile();
+        ArrayList<MusicGenre> musicGenres = MusicGenre.convertMusicBandsToGenres(musicBands);
+
+        Writer jsonWriter = Writer.create(jsonFileName);
+        jsonWriter.write(musicGenres);
+    }
+}
