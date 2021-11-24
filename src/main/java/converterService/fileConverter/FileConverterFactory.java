c0f816082676e5fdd11907ae29a437782f63b0fb
vegxer <vegxer@mail.ru>
2021-11-24 19:05:11 +0300

    if else заменён на более удобный switch

--- src/main/java/converterService/fileConverter/FileConverterFactory.java
+++ src/main/java/converterService/fileConverter/FileConverterFactory.java
@@ -7,11 +7,10 @@ import java.io.IOException;
 public class FileConverterFactory {
 
     public static FileConverter create(String fileName) throws IOException {
-        if (FilenameUtils.getExtension(fileName).equals("xml"))
-            return new XmlToJsonFileConverter(fileName);
-        else if (FilenameUtils.getExtension(fileName).equals("json"))
-            return new JsonToXmlFileConverter(fileName);
-        else
-            throw new IllegalArgumentException("Неверное расширение файла");
+        return switch (FilenameUtils.getExtension(fileName)) {
+            case "xml" -> new XmlToJsonFileConverter(fileName);
+            case "json" -> new JsonToXmlFileConverter(fileName);
+            default -> throw new IllegalArgumentException("Неверное расширение файла");
+        };
     }
 }
