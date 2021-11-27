80803478f202e62d06166f7d0d3f83a5afb76492
vegxer <vegxer@mail.ru>
2021-11-27 11:57:06 +0300

    Тест некорректных файлов при помощи логов

--- src/test/java/ru/itdt/converterService/IncorrectFilesTest.java
+++ src/test/java/ru/itdt/converterService/IncorrectFilesTest.java
@@ -1,76 +1,73 @@
 package ru.itdt.converterService;
 
-import com.google.gson.JsonParseException;
+import org.apache.commons.io.FileUtils;
 import org.json.simple.parser.ParseException;
-import org.junit.Rule;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import ru.itdt.converterService.fileConverter.FileConverterFactory;
 
-import javax.management.modelmbean.XMLParseException;
 import javax.xml.stream.XMLStreamException;
+import java.io.File;
 import java.io.IOException;
 
 public class IncorrectFilesTest {
     private static final String JSON_PATH = "src/test/resources/incorrectFiles/jsonFiles";
     private static final String XML_PATH = "src/test/resources/incorrectFiles/xmlFiles";
     private static final String OUTPUT_PATH = "src/test/resources/incorrectFiles/outputFiles";
-    @Rule
-    public final ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void emptyGenres() throws IOException, XMLStreamException, ParseException {
-        thrown.expect(XMLParseException.class);
-        thrown.expectMessage("Genres are not found");
         FileConverterFactory.create(XML_PATH + "/emptyGenres.xml").convert(OUTPUT_PATH + "/out.json");
+        Assert.assertEquals("Жанров не нашлось\r\n", FileUtils.readFileToString(
+                new File("xml reading log.txt"), "utf-8"));
     }
 
     @Test
     public void incorrectRootTag() throws IOException, XMLStreamException, ParseException {
-        thrown.expect(XMLParseException.class);
-        thrown.expectMessage("Bands tag must be first tag");
         FileConverterFactory.create(XML_PATH + "/incorrectRootTag.xml").convert(OUTPUT_PATH + "/out.json");
+        Assert.assertEquals("Тег Bands должен быть корневым тегом файла\r\n", FileUtils.readFileToString(
+                new File("xml reading log.txt"), "utf-8"));
     }
 
     @Test
     public void noGenresXML() throws IOException, XMLStreamException, ParseException {
-        thrown.expect(XMLParseException.class);
-        thrown.expectMessage("Not enough tags in Band tag");
         FileConverterFactory.create(XML_PATH + "/noGenres.xml").convert(OUTPUT_PATH + "/out.json");
+        Assert.assertEquals("Жанров не нашлось\r\n", FileUtils.readFileToString(
+                new File("xml reading log.txt"), "utf-8"));
     }
 
     @Test
     public void noBandName() throws IOException, XMLStreamException, ParseException {
-        thrown.expect(XMLParseException.class);
-        thrown.expectMessage("Not enough tags in Band tag");
         FileConverterFactory.create(XML_PATH + "/noBandName.xml").convert(OUTPUT_PATH + "/out.json");
+        Assert.assertEquals("Не найден тег 'name'\r\n", FileUtils.readFileToString(
+                new File("xml reading log.txt"), "utf-8"));
     }
 
     @Test
     public void emptyBands() throws IOException, XMLStreamException, ParseException {
-        thrown.expect(JsonParseException.class);
-        thrown.expectMessage("Empty array of bands");
         FileConverterFactory.create(JSON_PATH + "/emptyBands.json").convert(OUTPUT_PATH + "/out.xml");
+        Assert.assertEquals("Музыкальные группы не найдены\r\n", FileUtils.readFileToString(
+                new File("json reading log.txt"), "utf-8"));
     }
 
     @Test
     public void noCountry() throws IOException, XMLStreamException, ParseException {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("parameter 'country' of ru/itdt/converterService/music/MusicBand.setCountry must not be null");
         FileConverterFactory.create(JSON_PATH + "/noCountry.json").convert(OUTPUT_PATH + "/out.xml");
+        Assert.assertEquals("Ключ 'country' не найден\r\n", FileUtils.readFileToString(
+                new File("json reading log.txt"), "utf-8"));
     }
 
     @Test
     public void noGenreName() throws IOException, XMLStreamException, ParseException {
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("parameter 'name' of ru/itdt/converterService/music/MusicGenre.setName must not be null");
         FileConverterFactory.create(JSON_PATH + "/noGenreName.json").convert(OUTPUT_PATH + "/out.xml");
+        Assert.assertEquals("Ключ 'name' не найден\r\n", FileUtils.readFileToString(
+                new File("json reading log.txt"), "utf-8"));
     }
 
     @Test
     public void noGenresJSON() throws IOException, XMLStreamException, ParseException {
-        thrown.expect(JsonParseException.class);
-        thrown.expectMessage("Key genres is not found");
         FileConverterFactory.create(JSON_PATH + "/noGenres.json").convert(OUTPUT_PATH + "/out.xml");
+        Assert.assertEquals("Ключ 'genres' не найден\r\n", FileUtils.readFileToString(
+                new File("json reading log.txt"), "utf-8"));
     }
 }
